eaa221e617785e631dde3051ca884ba2ee450320
SquareOf_y <y6357@naver.com>
2024-10-09 17:25:32 +0900

    package 정리

--- Code_Review/최신기출/나무박멸_황윤영.py
+++ Code_Review/최신기출/나무박멸_황윤영.py
@@ -1,4 +1,146 @@
 """
+
+1차
+풀이 시간 : 1시간 34분
+시도 횟수 : 2회
+실행 시간 : 776 ms
+메모리 28 mb
+
+2차
+풀이 시간 : 1시간 5분
+시도 횟수 : 2회
+실행 시간 : 567ms => 오 빨라졌는데 ~~~  제초제 뿌린 애들 lst로 담아온 덕인듯
+메모리 28 mb
+
+실수 모음
+    - 제초제 카운팅 뿌린 시간으로 기록하고 현재 시간과 차이로 하려는데 틀림
+        아직도 원인 잘 모르겠음,, 그냥 무조건 1빼는 식으로 해야겠음 이건,,
+    - 변수명 실수 년도는 m인데 K라고 함;
+        이거 요즘 엄청 실수하네? 종이에 변수 뭐쓸건지 잘 적어두고 실수하지 말자
+            실수리스트 당첨..
+    - 잔재가 남을 때 step으로 기록한 경우 step-arr값 빼야하는데 반대로 뺌
+
+
+"""
+
+"""
+================ 2차 코드리뷰 =============
+총평 
+- 이 문제 재재풀이에서 제외
+- 분명 잔재 남기는 로직 맞는데 왜 틀렸는지 이상해서 다시 살펴보고 
+    빼기 반대로 한 것 발견했다 .. 실수리스트 업데이트하기 ㅠ
+
+"""
+"""
+
+
+1. 인접한 네 개의 칸 중 나무가 있는 칸의 수만큼 나무가 성장
+
+2. 기존에 있었던 나무들은 인접한 4개의 칸 중 벽, 다른 나무, 제초제 모두 없는 칸에 번식을 진행
+     각 칸의 나무 그루 수에서 총 번식이 가능한 칸의 개수만큼 나누어진 그루 수만큼 번식
+     번식의 과정은 모든 나무에서 동시에 일어나게 됩니다.
+
+
+3.각 칸 중 제초제를 뿌렸을 때 나무가 가장 많이 박멸되는 칸에 제초제를 뿌립니다
+    박멸되는 나무 수 같으면 행이 작은 순서대로 / 그것도 같으면 열이 작은 칸에
+
+    나무가 없는 칸에 제초제를 뿌리면 박멸되는 나무가 전혀 없는 상태로 끝
+    나무가 있는 칸에 제초제를 뿌리게 되면 4개의 대각선 방향으로 k칸만큼 전파
+        전파되는 도중 벽이 있거나 나무가 아얘 없는 칸이 있는 경우,
+        그 칸 까지는 제초제가 뿌려지며 그 이후의 칸으로는 제초제가 전파되지 않습니다.
+    제초제가 뿌려진 칸에는 c년만큼 제초제가 남아있다가 c+1년째가 될 때 사라지게 됩니다.
+    제초제가 뿌려진 곳에 다시 제초제가 뿌려지는 경우에는 새로 뿌려진 해로부터 다시 c년동안 제초제가 유지
+
+"""
+
+
+def oob(i, j):
+    return i < 0 or j < 0 or i >= N or j >= N
+
+def printa(string, arr):
+    print(f"==============={string}=====================")
+    for i in range(len(arr)):
+        print(arr[i])
+    print("===============================================")
+    print()
+
+N, M, K, C = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+killed_arr = [[0] * N for _ in range(N)]
+answer = 0
+DIR = (-1, 0), (0, 1), (1, 0), (0, -1)
+diagonal = (1, 1), (1, -1), (-1, 1), (-1, -1)
+
+for m in range(1, M + 1):
+
+    # 나무 성장
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] > 0:
+                cnt = 0
+                for di, dj in DIR:
+                    du, dv = i + di, j + dj
+                    if oob(du, dv): continue
+                    if arr[du][dv] > 0:
+                        cnt += 1
+                arr[i][j] += cnt
+    # 나무 번식
+    tmp = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] <= 0: continue
+            lst = []
+            for di, dj in DIR:
+                du, dv = i + di, j + dj
+                if oob(du, dv): continue
+                if arr[du][dv] == 0 and (killed_arr[du][dv] == 0 or m-killed_arr[du][dv] > C):
+                    lst.append((du, dv))
+            if not lst: continue
+            v = arr[i][j] // len(lst)
+            for r, c in lst:
+                tmp[r][c] += v
+
+    for i in range(N):
+        for j in range(N):
+            arr[i][j]+= tmp[i][j]
+
+    killed_cnt = -1
+    spray_R, spray_C = -1, -1
+    lst = []
+    # 제초제 뿌릴 위치 찾기
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] <0: continue  # 벽
+            cnt = arr[i][j]
+            tmp_lst = [(i, j)]
+            if cnt!=0:
+                for di, dj in diagonal:
+                    du, dv = i, j
+                    for k in range(K):
+                        du+=di
+                        dv+=dj
+                        if oob(du, dv):
+                            break
+                        if arr[du][dv] > 0: cnt += arr[du][dv]
+                        tmp_lst.append((du, dv))
+
+                        if arr[du][dv] <= 0:
+                            break
+            if cnt > killed_cnt:
+                killed_cnt = cnt
+                lst = tmp_lst[:]
+                spray_R = i
+                spray_C = j
+
+    # 정해진 위치에 제초제 뿌리기
+    for r, c in lst:
+        if arr[r][c] > 0: arr[r][c] = 0
+        killed_arr[r][c] = C
+    answer += killed_cnt
+print(answer)
+
+
+"""
 코드리뷰
 
 총 풀이시간 1시간 34분
